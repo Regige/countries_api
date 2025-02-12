18a43edf92b5ff078fed05a3537d6ed31429dcd7
Regige <144334703+Regige@users.noreply.github.com>
2025-02-12 17:42:19 +0100

    Added Quiz logic

--- src/app/quiz-page/quiz-page.component.ts
+++ src/app/quiz-page/quiz-page.component.ts
@@ -1,14 +1,71 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { BackBtnComponent } from '../shared/back-btn/back-btn.component';
+import { DataService } from '../services/data.service';
+import { Country } from '../interfaces/country';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quiz-page',
   standalone: true,
-  imports: [HeaderComponent, BackBtnComponent],
+  imports: [HeaderComponent, BackBtnComponent, FormsModule, CommonModule],
   templateUrl: './quiz-page.component.html',
   styleUrl: './quiz-page.component.scss'
 })
 export class QuizPageComponent {
+
+  ranCountry: any = {};
+  userInput: string = '';
+  filteredCountries: Country[] = [];
+
+  constructor(private data: DataService) {}
+
+  ngOnInit(): void {
+    this.data.allCountries = this.data.getCountriesFromSession();
+
+    this.ranCountry = this.getRandomCountry();
+    console.log(this.ranCountry);
+    
+  }
+
+
+  getRandomCountry() {
+    if (this.data.allCountries.length === 0) return null;
+
+    const randomIndex = Math.floor(Math.random() * this.data.allCountries.length);
+    const ranCountry = this.data.allCountries[randomIndex];
+
+    return ranCountry;
+  }
+
+
+  onInputChange(): void {
+      if (!this.userInput) {
+        this.filteredCountries = [];
+        return;
+      }
+
+      const lowerCaseInput = this.userInput.toLowerCase();
+      this.filteredCountries = this.data.allCountries.filter(country =>
+        country.name.toLowerCase().startsWith(lowerCaseInput)
+      );
+  }
+
+
+  selectCountry(country: Country): void {
+    this.userInput = country.name; 
+    this.filteredCountries = [];
+  }
+
+
+  // checkAnswer(selectedCountry: string): boolean {
+  //   const correctCountry = 'Germany'; // Beispiel für das korrekte Land
+  //   if (selectedCountry.toLowerCase() === correctCountry.toLowerCase()) {
+  //     return true; // Richtiges Land
+  //   }
+  //   return false; // Falsches Land
+  // }
+
 
 }
